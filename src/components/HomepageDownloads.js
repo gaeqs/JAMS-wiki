9089469adb56df5f52a43bf48f48d954ecf00a71
Gael Rial Costas <g.rial.2018@alumnos.urjc.es>
2022-02-26 21:06:03 +0100

    Add features and download sections.

---
+++ src/components/HomepageDownloads.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import styles from './HomepageDownloads.module.css';
+
+const FeatureList = [
+    {
+        title: 'Windows',
+        image: '/img/windows.png',
+        download: 'https://github.com/gaeqs/JAMS/releases/download/0.4-BETA-PRE-1/JAMS-0.4.exe',
+    },
+    {
+        title: 'MacOS',
+        image: '/img/mac.png',
+        download: 'https://github.com/gaeqs/JAMS/releases/download/0.4-BETA-PRE-1/JAMS-macos.zip',
+    },
+    {
+        title: 'Ubuntu',
+        image: '/img/ubuntu.png',
+        download: 'https://github.com/gaeqs/JAMS/releases/download/0.4-BETA-PRE-1/jams_0.4.BETA.SNAPSHOT-1_amd64.deb',
+    },
+    {
+        title: 'Other',
+        image: '/img/java.png',
+        download: 'https://github.com/gaeqs/JAMS/releases/download/0.4-BETA-PRE-1/JAMS-0.4.BETA.SNAPSHOT.jar',
+    },
+];
+
+function Download({title, image, download}) {
+    return (
+        <div className={['col', styles.downloadImageDiv].join(' ')}>
+            <a href={download}>
+                <img className={styles.downloadImage} src={image} alt={title}/>
+            </a>
+        </div>
+    )
+}
+
+export default function HomepageDownloads() {
+    return (
+        <section className={styles.downloads}>
+            <div className="container">
+                <h1>Downloads:</h1>
+                <div className='row'>
+                    {FeatureList.map((props, idx) => (
+                        <Download key={idx} idx={idx} {...props} />
+                    ))}
+                </div>
+            </div>
+        </section>
+    );
+}
--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -3,59 +3,80 @@ import clsx from 'clsx';
 import styles from './HomepageFeatures.module.css';
 
 const FeatureList = [
-  {
-    title: 'Easy to Use',
-    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
-    description: (
-      <>
-          JAMS was designed using modern technologies, making it easy to use for beginners.
-      </>
-    ),
-  },
-  {
-    title: 'Fully customizable',
-    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
-    description: (
-      <>
-          JAMS is fully customizable, allowing you to create new themes, translations and plugins.
-      </>
-    ),
-  },
-  {
-    title: 'Powerful simulator',
-    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
-    description: (
-      <>
-          Look into every aspect of your execution using a powerful and customizable simulator.
-      </>
-    ),
-  },
+    {
+        title: 'Easy to Use',
+        image: '/img/JAMS-1.png',
+        description: (
+            <>
+                JAMS was designed using modern technologies, making it easy to use for beginners.
+            </>
+        ),
+    },
+    {
+        title: 'Powerful simulator',
+        image: '/img/JAMS-2.png',
+        description: (
+            <>
+                Look into every aspect of your execution using a powerful and customizable simulator.
+            </>
+        ),
+    },
+    {
+        title: 'Fully customizable',
+        image: '/img/JAMS-3.png',
+        description: (
+            <>
+                JAMS is fully customizable, allowing you to create new themes, translations and plugins.
+            </>
+        ),
+    },
 ];
 
-function Feature({Svg, title, description}) {
-  return (
-    <div className={clsx('col col--4')}>
-      <div className="text--center">
-        <Svg className={styles.featureSvg} alt={title} />
-      </div>
-      <div className="text--center padding-horiz--md">
-        <h3>{title}</h3>
-        <p>{description}</p>
-      </div>
-    </div>
-  );
+function FeatureImage({image, title}) {
+    return (
+        <div className={clsx('col col--8')}>
+            <div className="text--center">
+                <img className={styles.featureSvg} alt={title} src={image}/>
+            </div>
+        </div>
+    )
+}
+
+function FeatureTitle({title, description}) {
+    return (
+        <div className="col col--4 text--center padding-horiz--md">
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </div>
+    )
+}
+
+function Feature({idx, image, title, description}) {
+    if ((idx & 1) === 0) {
+        return (
+            <div className="row row--align-center margin-top--lg">
+                <FeatureImage image={image} title={title}/>
+                <FeatureTitle title={title} description={description}/>
+            </div>
+        );
+    } else {
+        return (
+            <div className="row row--align-center margin-top--lg">
+                <FeatureTitle title={title} description={description}/>
+                <FeatureImage image={image} title={title}/>
+            </div>
+        )
+    }
 }
 
 export default function HomepageFeatures() {
-  return (
-    <section className={styles.features}>
-      <div className="container">
-        <div className="row">
-          {FeatureList.map((props, idx) => (
-            <Feature key={idx} {...props} />
-          ))}
-        </div>
-      </div>
-    </section>
-  );
+    return (
+        <section className={styles.features}>
+            <div className="container">
+                {FeatureList.map((props, idx) => (
+                    <Feature key={idx} idx={idx} {...props} />
+                ))}
+            </div>
+        </section>
+    );
 }
